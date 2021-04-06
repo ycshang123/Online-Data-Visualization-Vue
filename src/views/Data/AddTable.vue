9261a97c1e2ae970bd12551dbc7ab28721d35143
xiexiaoqian <2320832818@qq.com>
2021-04-06 17:47:52 +0800

    数据包-数据预览（静态）

--- src/views/Data/AddTable.vue
+++ src/views/Data/AddTable.vue
@@ -1,13 +1,15 @@
 <template>
     <!-- 先设置整体高度100%，撑满屏幕 -->
-    <div style="height: 100%">
+    <div style="height: 100%"
+         class="d-flex">
 
         <!-- 左侧列表部分 -->
         <div style="height: 100%; width: 19%; border-right: 0.5px solid #e0e0e0">
 
             <!-- 所选表名 -->
             <v-list-item>
-                <v-icon class="mr-4" @click="previousPage()">mdi-chevron-left</v-icon>
+                <v-icon class="mr-4"
+                        @click="previousPage()">mdi-chevron-left</v-icon>
                 <v-list-item-title class="title">{{folder.name}}</v-list-item-title>
             </v-list-item>
 
@@ -29,7 +31,7 @@
                     <v-list>
                         <v-list-item v-for="(option, index) in options"
                                      :key="index"
-                                      @click="nextPage(option.path)">
+                                     @click="nextPage(option.path)">
                             <v-list-item-title> {{ option.name }} </v-list-item-title>
                         </v-list-item>
                     </v-list>
@@ -37,14 +39,87 @@
             </div>
 
             <!-- 表名列表 -->
-            <div>
-                
+            <div class="mt-5">
+                <!-- 循环的这一行 -->
+                <v-list dense>
+                    <v-list-item-group>
+                        <v-list-item v-for="folder in folders"
+                                     :key="folder.title">
+                            <!-- 行左侧图标 -->
+                            <v-list-item-avatar size="20">
+                                <v-icon x-small
+                                        class="grey lighten-1"
+                                        dark>
+                                    mdi-table
+                                </v-icon>
+                            </v-list-item-avatar>
+                            <!-- 行右侧表名 -->
+                            <v-list-item-content>
+                                <v-list-item-title v-text="folder.title"></v-list-item-title>
+
+                            </v-list-item-content>
+
+                            <!-- <v-list-item-action>
+                        <v-btn icon>
+                            <v-icon color="grey lighten-1">mdi-information</v-icon>
+                        </v-btn>
+                    </v-list-item-action> -->
+                        </v-list-item>
+                    </v-list-item-group>
+
+                </v-list>
             </div>
         </div>
+        <!-- 右侧数据表预览部分 -->
+        <v-main style="height: 100%;"
+                class="pt-3">
+            <!-- 工具栏 -->
+            <v-card flat
+                    tile
+                    class="">
 
+                <v-card-title style="height: 10%"
+                              class="pt-2">
+                    <v-btn icon
+                           x-small
+                           class="mr-2">
+                        <v-icon>mdi-table</v-icon>
+                    </v-btn>
+                    <!-- 表名 -->
+                    {{folder.name}}
 
-        <!-- 右侧数据预览部分 -->
-        <div></div>
+                    <!-- 空间填充 -->
+                    <v-spacer></v-spacer>
+
+                    <!-- 搜索框 -->
+                    <v-text-field v-model="search"
+                                  append-icon="mdi-magnify"
+                                  label="Search"
+                                  single-line
+                                  hide-details
+                                  dense
+                                  class="shrink mr-3"></v-text-field>
+
+                    <!-- 编辑和创建组件按钮 -->
+                    <v-card flat class="d-flex px-2" width="20%">
+                        <v-btn small dark class="mr-3" color="#25354d" style="opacity:0.9">编辑</v-btn>
+                        <v-btn small dark color="#25354d" style="opacity:0.9">创建组件</v-btn>
+                    </v-card>
+
+                </v-card-title>
+
+                <!-- 表 -->
+                <v-data-table :headers="headers"
+                              :items="desserts"
+                              :sort-by="['calories', 'fat']"
+                              :sort-desc="[false, true]"
+                              multi-sort
+                              class="elevation-1"
+                              loading-text
+                              loading
+                              :search="search"></v-data-table>
+            </v-card>
+        </v-main>
 
     </div>
 </template>
@@ -53,7 +128,11 @@
 export default {
     data () {
         return {
+            // 前一个页面传过来的数据包名称
             folder: {},
+            // 数据表-搜索的关键字
+            search: '',
+            // 添加表的三种选项
             options: [
                 {
                     id: 1,
@@ -71,16 +150,127 @@ export default {
                     path: '/datalink'
                 },
             ],
+            // 左侧表名
+            folders: [
+                {
+                    subtitle: 'Jan 9, 2014',
+                    title: '一月全国数据表',
+                },
+                {
+                    subtitle: 'Jan 17, 2014',
+                    title: '二月全国数据表',
+                },
+                {
+                    subtitle: 'Jan 28, 2014',
+                    title: '三月全国数据表',
+                },
+            ],
+            // 静态表格
+            headers: [
+                {
+                    text: 'Dessert (100g serving)',
+                    align: 'start',
+                    sortable: false,
+                    value: 'name',
+                },
+                { text: 'Calories', value: 'calories' },
+                { text: 'Fat (g)', value: 'fat' },
+                { text: 'Carbs (g)', value: 'carbs' },
+                { text: 'Protein (g)', value: 'protein' },
+                { text: 'Iron (%)', value: 'iron' },
+            ],
+            desserts: [
+                {
+                    name: 'Frozen Yogurt',
+                    calories: 200,
+                    fat: 6.0,
+                    carbs: 24,
+                    protein: 4.0,
+                    iron: '1%',
+                },
+                {
+                    name: 'Ice cream sandwich',
+                    calories: 200,
+                    fat: 9.0,
+                    carbs: 37,
+                    protein: 4.3,
+                    iron: '1%',
+                },
+                {
+                    name: 'Eclair',
+                    calories: 300,
+                    fat: 16.0,
+                    carbs: 23,
+                    protein: 6.0,
+                    iron: '7%',
+                },
+                {
+                    name: 'Cupcake',
+                    calories: 300,
+                    fat: 3.7,
+                    carbs: 67,
+                    protein: 4.3,
+                    iron: '8%',
+                },
+                {
+                    name: 'Gingerbread',
+                    calories: 400,
+                    fat: 16.0,
+                    carbs: 49,
+                    protein: 3.9,
+                    iron: '16%',
+                },
+                {
+                    name: 'Jelly bean',
+                    calories: 400,
+                    fat: 0.0,
+                    carbs: 94,
+                    protein: 0.0,
+                    iron: '0%',
+                },
+                {
+                    name: 'Lollipop',
+                    calories: 400,
+                    fat: 0.2,
+                    carbs: 98,
+                    protein: 0,
+                    iron: '2%',
+                },
+                {
+                    name: 'Honeycomb',
+                    calories: 400,
+                    fat: 3.2,
+                    carbs: 87,
+                    protein: 6.5,
+                    iron: '45%',
+                },
+                {
+                    name: 'Donut',
+                    calories: 500,
+                    fat: 25.0,
+                    carbs: 51,
+                    protein: 4.9,
+                    iron: '22%',
+                },
+                {
+                    name: 'KitKat',
+                    calories: 500,
+                    fat: 26.0,
+                    carbs: 65,
+                    protein: 7,
+                    iron: '6%',
+                },
+            ],
         }
     },
     created () {
         this.folder = this.$route.query.folder
     },
     methods: {
-        nextPage(path){
+        nextPage (path) {
             this.$router.push(path)
         },
-        previousPage() {
+        previousPage () {
             this.$router.go(-1)
         }
     },
@@ -97,4 +287,6 @@ export default {
 .outline-right {
     border-right: 1px solid #25354d;
 }
+
+
 </style>
