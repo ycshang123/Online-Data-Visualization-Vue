ff36f2d44ba846f263e861bc1da55a4ab9908a44
xiexiaoqian <2320832818@qq.com>
2021-04-13 14:43:20 +0800

    存表

--- src/views/Data/AddTable.vue
+++ src/views/Data/AddTable.vue
@@ -6,7 +6,7 @@
             <!-- 所选包名 -->
             <v-list-item>
                 <v-icon class="mr-4" @click="toDatapage()">mdi-chevron-left</v-icon>
-                <v-list-item-title class="title">{{ folder }}</v-list-item-title>
+                <v-list-item-title class="title">{{ folder.name }}</v-list-item-title>
             </v-list-item>
 
             <!-- 分割线 -->
@@ -32,14 +32,14 @@
                         </v-menu>
                     </div>
                     <v-list-item-group>
-                        <v-list-item v-for="table in allTables" :key="table.title" @click="getTable(table)">
+                        <v-list-item v-for="(table, index) in allTables" :key="index" @click="getTable(table)">
                             <!-- 行左侧图标 -->
                             <v-list-item-avatar size="20">
                                 <v-icon x-small class="grey lighten-1" dark> mdi-table </v-icon>
                             </v-list-item-avatar>
                             <!-- 行右侧表名 -->
                             <v-list-item-content>
-                                <v-list-item-title v-text="table.title"></v-list-item-title>
+                                <v-list-item-title v-text="table"></v-list-item-title>
                             </v-list-item-content>
                         </v-list-item>
                     </v-list-item-group>
@@ -81,7 +81,7 @@
                         <v-icon>mdi-table</v-icon>
                     </v-btn>
                     <!-- 表名 -->
-                    {{ table.title }}
+                    {{ table }}
 
                     <!-- 空间填充 -->
                     <v-spacer></v-spacer>
@@ -131,7 +131,7 @@
                     <v-spacer></v-spacer>
                     <!-- 已选择（）项 -->
                     <v-card flat class="d-flex align-center mr-4" max-height="40">
-                        <v-card-subtitle>已选择{{ selectedTables.length }}项</v-card-subtitle>
+                        <v-card-subtitle>已选择{{ allTables.length }}项</v-card-subtitle>
                     </v-card>
                     <!-- 取消和确定按钮 -->
                     <v-card flat class="d-flex px-2" width="20%">
@@ -205,7 +205,7 @@ export default {
             ],
             // 左侧表名,用户添加的所有的表
             allTables: [
-                { id: 0, title: '一月全国数据表' },
+                // { id: 0, title: '一月全国数据表' },
                 // {
                 //     id: 1,
                 //     title: '二月全国数据表',
@@ -264,16 +264,18 @@ export default {
     },
     methods: {
         /**
-         * @description: 构建用户选择的所有的表
+         * @description: 确定按钮点击事件，构建用户选择的所有的表
          * @param {*}
          * @return {*}
          */
         pushAllTables() {
-            this.allTables.push(this.selectedTables)
+            this.isShowOther = false
+            // this.allTables.push(this.selectedTables)
+            this.$store.commit('saveAllTables', JSON.stringify(this.allTables))
             console.log(this.allTables)
         },
         /**
-         * @description: 获取当前点击的表名
+         * @description: 左侧部分，获取当前点击的表名
          * @param {*} o
          * @return {*}
          */
@@ -300,8 +302,8 @@ export default {
                     const conn = this.historyConnArr[0]
                     await getConnTables(conn).then((res) => {
                         if (res.code === 200) {
-                            console.log('请求接口：' + res.data)
-                            console.log(conn)
+                            console.log('当前连接中所有的表：' + res.data)
+                            console.log('当前连接对象：' + conn)
                             this.$store.commit('saveConnTables', res.data)
                         }
                     })
@@ -319,8 +321,16 @@ export default {
          * 每个表名按钮的点击事件 => 选择表
          */
         selectTable(o) {
-            this.selectedTables.push(o)
-            console.log(this.selectedTables)
+            const allTables = this.allTables
+            allTables.push(o)
+            // allTables.forEach((element) => {
+            //     if (element != o) {
+            //         allTables.push(o)
+            //     }else {
+            //         alert('请勿重复选择')
+            //     }
+            // })
+            console.log(this.allTables)
         },
         /**
          * 连接名的点击事件
