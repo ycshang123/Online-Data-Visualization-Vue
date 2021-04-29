0b65b46cb8687fb58033d481da78e51338664c34
xiexiaoqian <2320832818@qq.com>
2021-04-29 10:53:17 +0800

    修复添加表按钮

--- src/views/Data/AddTable.vue
+++ src/views/Data/AddTable.vue
@@ -36,8 +36,8 @@
                             <!-- 点击按钮出现的，三种选项 -->
                             <v-list>
                                 <v-list-item
-                                    v-show="option.isShow"
                                     v-for="(option, index) in options"
+                                    v-show="option.isShow"
                                     :key="index"
                                     @click="nextPage(option.show)"
                                 >
@@ -82,10 +82,10 @@
         <!-- 右侧部分 -->
         <v-main style="height: 100%; width: 80%" class="pt-1" v-relative>
             <!-- 数据表预览 -->
-            <v-card v-if="!isShowOther && allTables.length !== 0" flat tile max-width="99%" height="100%" v-absolute >
+            <v-card v-if="!isShowOther && allTables.length !== 0" flat tile max-width="99%" height="100%" v-absolute>
                 <!-- 工具栏 -->
                 <!-- 表名图标 -->
-                <v-card-title style="height: 10%" class="pt-2 subtitle-2 ">
+                <v-card-title style="height: 10%" class="pt-2 subtitle-2">
                     <v-btn icon x-small class="mr-2">
                         <v-icon>mdi-table</v-icon>
                     </v-btn>
@@ -217,27 +217,9 @@ export default {
             // 每个连接中所有的表
             connTables: [],
             // 表头
-            headers: [
-                // { text: 'Dessert (100g serving)', align: 'start', sortable: false, value: 'name' },
-                // { text: 'Calories', value: 'calories' },
-                // { text: 'Fat (g)', value: 'fat' },
-                // { text: 'Carbs (g)', value: 'carbs' },
-                // { text: 'Protein (g)', value: 'protein' },
-                // { text: 'Iron (%)', value: 'iron' },
-            ],
+            headers: [],
             // 记录
-            desserts: [
-                // { name: 'Frozen Yogurt', calories: 200, fat: 6.0, carbs: 24, protein: 4.0, iron: '1%' },
-                // { name: 'Ice cream sandwich', calories: 200, fat: 9.0, carbs: 37, protein: 4.3, iron: '1%' },
-                // { name: 'Eclair', calories: 300, fat: 16.0, carbs: 23, protein: 6.0, iron: '7%' },
-                // { name: 'Cupcake', calories: 300, fat: 3.7, carbs: 67, protein: 4.3, iron: '8%' },
-                // { name: 'Gingerbread', calories: 400, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%' },
-                // { name: 'Jelly bean', calories: 400, fat: 0.0, carbs: 94, protein: 0.0, iron: '0%' },
-                // { name: 'Lollipop', calories: 400, fat: 0.2, carbs: 98, protein: 0, iron: '2%' },
-                // { name: 'Honeycomb', calories: 400, fat: 3.2, carbs: 87, protein: 6.5, iron: '45%' },
-                // { name: 'Donut', calories: 500, fat: 25.0, carbs: 51, protein: 4.9, iron: '22%' },
-                // { name: 'KitKat', calories: 500, fat: 26.0, carbs: 65, protein: 7, iron: '6%' },
-            ],
+            desserts: [],
         }
     },
     created() {
@@ -319,7 +301,7 @@ export default {
                     data.forEach((element) => {
                         let obj = {}
                         let i = 0
-                        console.log(element);
+                        console.log(element)
                         headers.forEach((item) => {
                             obj[item.text] = element[i]
                             i++
@@ -336,7 +318,8 @@ export default {
          * @return {*}
          */
         addBtnClick() {
-            if (this.allTables != null) {
+            console.log(this.allTables)
+            if (this.allTables.length != 0) {
                 this.options[2].isShow = true
             }
         },
